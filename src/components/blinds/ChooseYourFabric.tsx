5f9caebfffd83b3bfad7077e7ab6bcbe80ca3575
Ameer12348 <ameerarif12348@gmail.com>
2025-03-17 16:01:59 +0500

    added border to selected fabric in chooseyourfabrics

--- src/components/blinds/ChooseYourFabric.tsx
+++ src/components/blinds/ChooseYourFabric.tsx
@@ -53,7 +53,7 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
         className="mx-auto mb-20 aspect-[1156/628] max-w-[1156px] rounded-[74px]"
         style={{ background: currentFabric.color }}
       ></div>
-      <div className="flex max-w-full flex-col justify-center [column-gap:30px] [row-gap:40px] xl:flex-row xl:items-stretch">
+      <div className="flex max-w-full flex-col justify-center [column-gap:30px] [row-gap:40px] xl:flex-row xl:items-end">
         <div className="flex flex-col items-center gap-11">
           <div
             className={`${rubik.className} text-center text-2xl font-semibold text-[#023D64]`}
@@ -64,13 +64,16 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
             opts={{
               align: "start",
               loop: true,
+              skipSnaps: true,
+              containScroll: "trimSnaps",
+              dragFree: false,
             }}
             className="flex w-full items-center gap-5 [&>*:nth-child(2)]:flex-grow"
           >
             <CarouselPrevious className="!static rounded-full bg-white text-3xl text-[#FFBB3D] duration-300 hover:opacity-100">
               <FaCircleChevronLeft />
             </CarouselPrevious>
-            <CarouselContent className="-ml-2 md:-ml-4">
+            <CarouselContent className="">
               {data.blackoutFabrics.map((fabric, index) => (
                 <CarouselItem
                   key={index}
@@ -79,7 +82,11 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
                   <div className="flex justify-center">
                     <div
                       onClick={() => setCurrentFabric(fabric)}
-                      className="flex aspect-[154/184] max-w-[160px] cursor-pointer flex-col items-center justify-end rounded-[34px] p-6"
+                      className={`flex aspect-[154/184] max-w-[160px] cursor-pointer flex-col items-center justify-end rounded-[34px] p-6 ${
+                        currentFabric.color === fabric.color
+                          ? "border-4 border-[#FFBB3D]"
+                          : ""
+                      }`}
                       style={{ background: fabric.color }}
                     >
                       <h3
@@ -97,6 +104,9 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
             </CarouselNext>
           </Carousel>
         </div>
+        <div className="hidden h-full xl:block">
+          <div className="h-[191px] w-[2.5px] bg-[#A39F9F]"></div>
+        </div>
         <div className="flex flex-col items-center gap-11">
           <div
             className={`${rubik.className} text-center text-2xl font-semibold text-[#023D64]`}
@@ -107,13 +117,16 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
             opts={{
               align: "start",
               loop: true,
+              skipSnaps: true,
+              containScroll: "trimSnaps",
+              dragFree: false,
             }}
             className="flex w-full items-center gap-5 [&>*:nth-child(2)]:flex-grow"
           >
             <CarouselPrevious className="!static rounded-full bg-white text-3xl text-[#FFBB3D] duration-300 hover:opacity-100">
               <FaCircleChevronLeft />
             </CarouselPrevious>
-            <CarouselContent className="-ml-2 md:-ml-4">
+            <CarouselContent className="">
               {data.lightoutFabrics.map((fabric, index) => (
                 <CarouselItem
                   key={index}
@@ -122,7 +135,11 @@ const ChooseYourFabric: React.FC<Props> = ({ data }) => {
                   <div className="flex justify-center">
                     <div
                       onClick={() => setCurrentFabric(fabric)}
-                      className="flex aspect-[154/184] max-w-[160px] cursor-pointer flex-col items-center justify-end rounded-[34px] p-6"
+                      className={`flex aspect-[154/184] max-w-[160px] cursor-pointer flex-col items-center justify-end rounded-[34px] p-6 ${
+                        currentFabric.color === fabric.color
+                          ? "border-4 border-[#FFBB3D]"
+                          : ""
+                      }`}
                       style={{ background: fabric.color }}
                     >
                       <h3
